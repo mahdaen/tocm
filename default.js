7b883c2db619aaf7511a1f9208ae17450930aa0e
Nanang Mahdaen EA <mahdaen@yahoo.co.id>
2013-12-15 05:22:12 +0700

    Initial update

--- default.js
+++
@@ -1,5 +0,0 @@
-/*jshint undef:false*/
-
-// Calling Tocm scripts.
-require(['source/native', 'source/tocmreg', 'source/tocmref', 'source/builder', 'source/extension']);
-require(['source/global', 'source/tocm', 'test/default.css']);
